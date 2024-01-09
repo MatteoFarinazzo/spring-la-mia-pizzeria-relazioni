08abae76b469f549608244013611bf08083d6402
MatteoFarinazzo <matteofary@gmail.com>
2024-01-09 17:28:32 +0100

    still working on style

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO pizzas (nome, descrizione, foto, prezzo) VALUES('Diavola', 'Base rossa, pomodoro del piennolo, mozzarella di bufala, salame piccante, provola affumicata' , 'https://freepngimg.com/download/pizza/35730-9-pepperoni-pizza-image.png', 9.50);
 INSERT INTO pizzas (nome, descrizione, foto, prezzo) VALUES('Margherita', 'Base rossa, pomodoro del piennolo, mozzarella di , basilico' , 'https://www.bora.com/fileadmin/website_content/Rezepte/X_BO_Rezepte/X_BO_Automatikrezepte/Pizza.jpg', 7.00);
-INSERT INTO pizzas (nome, descrizione, foto, prezzo) VALUES('Patate e salsiccia', 'Base bianca, Patate gialle, mozzarella di bufala, Salsiccia della fattoria Milani' , ${/patate.png}, 8.00);
+INSERT INTO pizzas (nome, descrizione, foto, prezzo) VALUES('Patate e salsiccia', 'Base bianca, Patate gialle, mozzarella di bufala, Salsiccia della fattoria Milani' , '/patate.png', 8.00);
 
