3eff8538ab0da31957f7e9b2f983daed90bed064
MatteoFarinazzo <matteofary@gmail.com>
2024-01-18 15:55:22 +0100

    created create/edit page

--- src/main/java/org/learning/springpizzeria/controller/IngredientController.java
+++ src/main/java/org/learning/springpizzeria/controller/IngredientController.java
@@ -2,15 +2,18 @@ package org.learning.springpizzeria.controller;
 
 import jakarta.validation.Valid;
 import org.learning.springpizzeria.model.Ingredient;
+import org.learning.springpizzeria.model.Pizza;
 import org.learning.springpizzeria.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -32,12 +35,53 @@ public class IngredientController {
     }
 
     @PostMapping("/create")
-    public String create(@Valid @ModelAttribute("formIngredient") Ingredient formIngredient, BindingResult bindingResult) {
+    public String ingrediente(@Valid @ModelAttribute("formIngredient") Ingredient formIngredient, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
+            model.addAttribute("ingredienti", ingredientRepository.findAll());
             return "ingredients/create";
         } else {
-            ingredientRepository.save(formIngredient);
+            Ingredient saved = ingredientRepository.save(formIngredient);
             return "redirect:/ingredients";
+        }
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Optional<Ingredient> result = ingredientRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("ingredient", result.get());
+            return "ingredients/create";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ingredient with id" + id + "not found");
+        }
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable Integer id, @Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult bindingResult) {
+
+        Optional<Ingredient> result = ingredientRepository.findById(id);
+        if (result.isPresent()) {
+            Ingredient ingredientToEdit = result.get();
+            if (bindingResult.hasErrors()) {
+                return "ingredients/create";
+            }
+
+            Ingredient savedIngredient = ingredientRepository.save(formIngredient);
+            return "redirect:/ingredients";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ingredient with id" + id + "not found");
+        }
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        Optional<Ingredient> result = ingredientRepository.findById(id);
+        if (result.isPresent()) {
+            ingredientRepository.deleteById(id);
+            redirectAttributes.addFlashAttribute("redirectMessage", "l'ingrediente " + result.get().getName() + " è stato eliminato");
+            return "redirect:/ingredients";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "L'ingrediente con id: " + id + "non è stato trovato");
         }
     }
 }
